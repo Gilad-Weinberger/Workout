7391546ad9ca4926b7726a0667d03a6c52520396
Gilad Weinberger <giladwe21@gmail.com>
2024-12-19 23:54:03 +0200

    deploy update 6

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://workout-server-brown.vercel.app/",
         changeOrigin: true,
         secure: false,
       },
